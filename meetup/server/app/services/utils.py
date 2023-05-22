16370087063de9dffafeab65797f2ab19204dd18
fatjun <i.fatjuni@gmail.com>
2023-05-22 16:32:29 +0600

    remove useless imports from file

--- meetup/server/app/services/utils.py
+++ meetup/server/app/services/utils.py
@@ -1,9 +1,3 @@
-import bcrypt
-import httpx
-
-from config import WEBHOOK_API_TOKEN
-
-
 def generate_response(msg: str = "", *, success: bool = True, status_code: int = 200, **payload):
     return {"msg": msg, "success": success, "status_code": status_code, "payload": payload}
 
